d65e0805996f931b4dc2769cbffc064be10d843e
Fien Gérardi <fien.gerardi@gmail.com>
2021-03-10 19:58:36 +0100

    position notes array changed and function remove added

--- lab3/app.js
+++ lab3/app.js
@@ -1,92 +1,79 @@
-notes = [];
-
 class Note {
-    constructor(title) {
+    constructor(parent, title) {
+      this.parent = parent;
       this.title = title;
       this.element = this.createElement(title);
     }
   
     createElement(title) {
+      //create new list item
       let newNote = document.createElement("li");
       newNote.innerHTML = title;
-      newNote.addEventListener('click', this.remove.bind(newNote));
+      newNote.addEventListener('click', this.remove.bind(this));
       return newNote;
     }
   
     add() {
-      // HINT🤩
-      // this function should append the note to the screen somehow
-      let list = document.querySelector("#taskList");
-      list.appendChild(this.element);
-      this.saveToStorage();
+      let list = document.querySelector("#taskList"); // select list
+      list.appendChild(this.element); // append note to screen
     }
   
     saveToStorage() {
-      // HINT🤩
-      // localStorage only supports strings, not arrays
-      // if you want to store arrays, look at JSON.parse and JSON.stringify
-      let notesTitles = [];
-      for (let i = 0; i < notes.length; i++) {
-        notesTitles.push(notes[i].title);
-      }
-      localStorage.setItem('notes', JSON.stringify(notesTitles));
+      const jsonArr = JSON.stringify(this.parent.notes);
+      localStorage.setItem('notes', jsonArr);
     }
   
     remove() {
-      // HINT🤩 the meaning of 'this' was set by bind() in the createElement function
-      // in this function, 'this' will refer to the current note element
-      //this.parentNode.removeChild(this);
-      // remove the item from screen and from localstorage
-      //console.log("remove");
-      //localStorage.removeItem(note);
-
+      for (let i = 0; i < this.parent.notes.length; i++) {
+        if (this.title === this.parent.notes[i]) {
+          this.parent.notes.splice(i, 1);
+        }
+      }    
+      this.saveToStorage();
+      this.element.parentNode.removeChild(this.element); //remove from ul
     }
   }
-
   
   class App {
-    
     constructor() {
       console.log("👊🏼 The Constructor!");
+      this.notes = [];
       this.txtTodo = document.querySelector("#taskInput");
       this.list = document.querySelector("#taskList");
       this.txtTodo.addEventListener("keypress", this.createNote.bind(this)); // pressing the enter key in the text field triggers the createNote function
-
-      // read up on .bind() -> we need to pass the current meaning of this to the eventListener
-      // when the app loads, we can show previously saved noted from localstorage
       this.loadNotesFromStorage();
     }
   
     loadNotesFromStorage() {
-      // HINT🤩
       // load all notes from storage here and add them to the screen
       if (localStorage.getItem("notes") !== null) {
-        let notesFromStorage = JSON.parse(localStorage.getItem("notes")); //fill array
-      
-        for (let i = 0; i < notesFromStorage.length; i++) {
-          console.log(notesFromStorage[i]);
-          this.newNote(notesFromStorage[i]);
+        const str = localStorage.getItem("notes");
+        let parsedNotes = JSON.parse(str); //fill array
+
+        for (let i = 0; i < parsedNotes.length; i++) {
+          this.newNote(parsedNotes[i]);
         }
       }
     }
 
     newNote(title) {
-      const note = new Note(title); // create a new note by using the Note() class
-      notes.push(note); //push note to array
+      const note = new Note(this, title); // create a new note by using the Note() class
+      const noteTitle = note.title;
+      this.notes.push(noteTitle); //push note to array
       note.add(); // note.add();
+      note.saveToStorage(); // when adding note, save to local storage storage
     }
   
     createNote(e) {
       if(e.key === "Enter"){
         this.newNote(this.txtTodo.value);
         this.reset(); // clear text field
-        // note.saveToStorage();
         e.preventDefault();
       }      
     }
   
     reset() {
-      // this function should reset the form / clear the text field
+      // clear the text field when ENTER
       this.txtTodo.value = "";
     }
   }
