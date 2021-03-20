2ec0d6fbf0405eb2e1dcce1742deae1e440f7244
Fien Gérardi <fien.gerardi@gmail.com>
2021-03-20 18:53:15 +0100

    pug file added

--- lab5/index.js
+++ lab5/index.js
@@ -2,8 +2,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const messagesRouter = require('./routers/api/v1/messages');
+const pug = require('pug');
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.set('view engine', 'pug');
+app.get('/', (req, res) => res.render('index'));
+
 app.use('/api/v1/messages', messagesRouter);
 
 app.listen(port, () => {
