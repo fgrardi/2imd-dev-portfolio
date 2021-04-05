223c024935d42a7fbc9abc3efcb0a8428610d583
Fien Gérardi <fien.gerardi@gmail.com>
2021-04-05 12:30:11 +0200

    fix: use cors as middleware

--- lab5/index.js
+++ lab5/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 3000;
 const messagesRouter = require('./routers/api/v1/messages');
 const pug = require('pug');
+
+app.use(cors());
 
 app.set('view engine', 'pug');
 app.get('/', (req, res) => res.render('index'));
